81cd0a6164bfb9bdcfd4778864925fd4db50b085
王金耀 <wjyzzuer@163.com>
2024-09-27 17:30:49 +0800

    feat: 抽象为MapUtil类，修改点位加载及初始化实例方法

---
+++ docs/views/amap/amapData.vue
@@ -0,0 +1,62 @@
+<!--
+ * @Author: Wang_Jinyao && wjyzzuer@163.com
+ * @Date: 2024-07-24 16:08:11
+ * @LastEditors: Wang_Jinyao && wjyzzuer@163.com
+ * @LastEditTime: 2024-09-27 11:25:34
+ * @FilePath: \code\docs\views\amap\amapData.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by Wang_Jinyao, All Rights Reserved. 
+-->
+<script setup>
+import { onMounted, onUnmounted } from "vue";
+
+let mapUtil
+onMounted(() => {
+  import('../../src/map').then(module => {
+    mapUtil = new module.MapUtil('高德')
+    mapUtil.initMap({
+      center: [116.397428, 39.90923],
+      zoom: 12,
+      heading: -15,
+      tilt: 45,
+      callback: () => {
+        loadPointLayerDemo()
+      }
+    })
+  })
+})
+
+onUnmounted(() => {
+  mapUtil.destroyMap()
+})
+const loadPointLayerDemo = () => {
+  mapUtil.loadPointLayer({
+    layerid: 'pointLayer',
+    offsetX: -20,
+    offsetY: -50,
+    iconCfg: {
+      url:'../src/img/icon.png',
+      width: 40,
+      height: 50
+    },
+    data: [
+      { lng: 116.397428, lat: 39.90923, name: 'point1' },
+      { lng: 116.458428, lat: 39.90923, name: 'point2' },
+      { lng: 116.366428, lat: 39.91923, name: 'point3' }
+    ]
+  })
+}
+
+</script>
+
+<template>
+  <div id="container"></div>
+</template>
+
+<style scoped>
+#container {
+  width: 100%;
+  height: 400px;
+}
+</style>
